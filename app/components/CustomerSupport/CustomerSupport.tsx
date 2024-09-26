c67aca34cbf561811a1e0d83c3e5be8f46735abe
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2024-09-26 10:15:29 +0300

    Feature List and customer support section completed

--- app/components/CustomerSupport/CustomerSupport.tsx
+++ app/components/CustomerSupport/CustomerSupport.tsx
@@ -1,11 +1,62 @@
 import React from 'react'
+import SupportImg from '@/public/support.jpg'
+import Image from 'next/image'
+import { CheckIcon } from '@heroicons/react/16/solid'
 
-const CustomerSuppert = () => {
+const CustomerSupport = () => {
   return (
     <div>
-      
+      <div className='pt-[5rem] pb-[3rem]'>
+        <div className='w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-[4rem] items-center'>
+          <div className='order-2 lg:order-1'>
+            <Image src={SupportImg} alt='support'/>
+          </div>
+          <div className='order-1 lg:order-2'>
+            <h1 className='text-[20px] md:text-[25] lg:text-[28px] text-[#02073e] font-bold leading-[2rem] 
+            md:leading-[3rem]'>
+              Customer support is our main priority with their hundred percent satisfaction
+            </h1>
+            <p className='mt-[1.5rem] mb-[1.5rem] text-black opacity-90 text-[15px] md:text-[16px] '>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti nam ipsum esse ad doloribu
+              s fuga repellendus recusandae aliquam ducimus tempore, ipsa quisquam quod nostrum facere laudantium 
+              a doloremque distinctio incidunt.
+            </p>
+            <div className='flex items-center mb-[1rem] space-x-3 '>
+              <CheckIcon className='w-[1.3rem] h-[1.3rem] text-red-600'/>
+              <p className='text-[17px] text-[#02073e] font-[500]'>
+                Visa and Passport
+              </p>
+            </div>
+            <div className='flex items-center mb-[1rem] space-x-3 '>
+              <CheckIcon className='w-[1.3rem] h-[1.3rem] text-red-600'/>
+              <p className='text-[17px] text-[#02073e] font-[500]'>
+                Visa and Passport
+              </p>
+            </div>
+            <div className='flex items-center mb-[1rem] space-x-3 '>
+              <CheckIcon className='w-[1.3rem] h-[1.3rem] text-red-600'/>
+              <p className='text-[17px] text-[#02073e] font-[500]'>
+                Visa and Passport
+              </p>
+            </div>
+            <div className='flex items-center mb-[1rem] space-x-3 '>
+              <CheckIcon className='w-[1.3rem] h-[1.3rem] text-red-600'/>
+              <p className='text-[17px] text-[#02073e] font-[500]'>
+                Visa and Passport
+              </p>
+            </div>
+            <div className='flex items-center mb-[1rem] space-x-3 '>
+              <CheckIcon className='w-[1.3rem] h-[1.3rem] text-red-600'/>
+              <p className='text-[17px] text-[#02073e] font-[500]'>
+                Visa and Passport
+              </p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default CustomerSuppert
+export default CustomerSupport
--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import Features from './Features/Features'
-import CustomerSuppert from './CustomerSupport/CustomerSupport'
+import CustomerSupport from './CustomerSupport/CustomerSupport'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <Features/>
-      <CustomerSuppert/>
+      <CustomerSupport/>
     </div>
   )
 }
